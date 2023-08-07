78bcc2a40406e1c3560ad67c4e703a9bc6c9fd5d
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-07 15:29:06 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using EmployeeLib;
+
+namespace XMLSerializationImplement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 11,
+                FirstName = "Kamarthi",
+                LastName = "Bhoomika",
+                Salary = 50000.00
+            };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            using (TextWriter writer = new StreamWriter("C:\\Users\\bonam\\OneDrive\\Desktop\\Basicapp\\Date7-8\\employee.xml"))
+            {
+                serializer.Serialize(writer, employee);
+                Console.WriteLine("De-Serialization");
+                Console.WriteLine($"Id: {employee.Id}");
+                Console.WriteLine($"First Name: {employee.FirstName}, Last Name: {employee.LastName}");
+                Console.WriteLine($"Salary: {employee.Salary}");
+            }
+            using (TextReader reader = new StreamReader("D:\\Mphasis\\.net\\Day21\\Assignment24\\employee.xml"))
+            {
+                Employee deserialize = (Employee)serializer.Deserialize(reader);
+                Console.WriteLine("De-Serialization");
+                Console.WriteLine($"Id: {deserialize.Id}");
+                Console.WriteLine($"First Name: {deserialize.FirstName}, Last Name: {deserialize.LastName}");
+                Console.WriteLine($"Salary: {deserialize.Salary}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
